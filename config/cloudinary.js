26bfecc0e604677d801d7a8b0e0d96932051443a
marckstar-RjJ <202001810@est.umss.edu>
2025-05-08 01:10:07 -0400

    arreglo imagen final

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: 'defwxebhk',
-  api_key: '61441519441725',
+  api_key: '614415194411725',
   api_secret: 'DHd8tTNfa0oJjdFHTV5u4JHsA8s'
 });
 
